0e94a3808e450a9cebd61130c21a0aa605ce41a4
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-04-12 23:57:10 +0100

    Bash, testApp

--- src/TestApp.java
+++ src/TestApp.java
@@ -2,7 +2,7 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class App {
+public class TestApp {
 	
 	public static void main(String[] args) {
 
--- src/demo.sh
+++ src/demo.sh
@@ -1,39 +1,39 @@
-java App localhost:Peer1 RECLAIM 10000000
+java TestApp localhost:Peer1 RECLAIM 10000000
 
-java App localhost:Peer2 RECLAIM 10000000
+java TestApp localhost:Peer2 RECLAIM 10000000
 
-java App localhost:Peer3 RECLAIM 10000000
+java TestApp localhost:Peer3 RECLAIM 10000000
 
-java App localhost:Peer1 BACKUP paisagem.jpg 1
+java TestApp localhost:Peer1 BACKUP paisagem.jpg 1
 
-java App localhost:Peer1 STATUS
+java TestApp localhost:Peer1 STATUS
 
-java App localhost:Peer2 STATUS
+java TestApp localhost:Peer2 STATUS
 
-java App localhost:Peer3 STATUS
+java TestApp localhost:Peer3 STATUS
 
-java App localhost:Peer1 RESTORE paisagem.jpg
+java TestApp localhost:Peer1 RESTORE paisagem.jpg
 
-java App localhost:Peer1 DELETE paisagem.jpg
+java TestApp localhost:Peer1 DELETE paisagem.jpg
 
-java App localhost:Peer1 STATUS
+java TestApp localhost:Peer1 STATUS
 
-java App localhost:Peer2 STATUS
+java TestApp localhost:Peer2 STATUS
 
-java App localhost:Peer3 STATUS
+java TestApp localhost:Peer3 STATUS
 
-java App localhost:Peer2 RECLAIM 65000
+java TestApp localhost:Peer2 RECLAIM 65000
 
-java App localhost:Peer3 BACKUP bigmac.jpg 2
+java TestApp localhost:Peer3 BACKUP bigmac.jpg 2
 
-java App localhost:Peer2 RECLAIM 0
+java TestApp localhost:Peer2 RECLAIM 0
 
-java App localhost:Peer1 STATUS
+java TestApp localhost:Peer1 STATUS
 
-java App localhost:Peer2 STATUS
+java TestApp localhost:Peer2 STATUS
 
-java App localhost:Peer3 STATUS
+java TestApp localhost:Peer3 STATUS
 
-java App localhost:Peer3 RESTORE bigmac.jpg
+java TestApp localhost:Peer3 RESTORE bigmac.jpg
 
-java App localhost:Peer3 DELETE bigmac.jpg
+java TestApp localhost:Peer3 DELETE bigmac.jpg
